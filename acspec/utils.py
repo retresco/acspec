ce06ed0b8d45657135e7b0f446116453e36d34df
Michael Jurke <fairnmicha@gmx.de>
2016-11-20 16:29:34 +0100

    Add test_utils

--- acspec/utils.py
+++ acspec/utils.py
@@ -7,13 +7,6 @@ from six import iteritems
 from acspec.dsl import has_option, get_option
 
 
-def _get_bases(spec):
-    if has_option(spec, "bases"):
-        return set(get_option(spec, "bases"))
-    else:
-        return set()
-
-
 def topological_iteritems(specs, pre_emitted=[]):
     pending = [(name, _get_bases(spec)) for name, spec in iteritems(specs)]
     emitted = pre_emitted[:]
@@ -30,9 +23,36 @@ def topological_iteritems(specs, pre_emitted=[]):
                 emitted.append(name)
                 next_emitted.append(name)
         if not next_emitted:
-            raise ValueError("Cyclic inheritance: {}".format(next_pending))
+            raise ValueError("Cyclic inheritance: {}".format(
+                _print_cycle(next_pending)
+            ))
         pending = next_pending
         emitted = next_emitted
+
+
+def _get_bases(spec):
+    if has_option(spec, "bases"):
+        return set(get_option(spec, "bases"))
+    else:
+        return set()
+
+
+def _print_cycle(pending):
+    names = [name for name, _ in pending]
+    def relevant_base(bases):
+        bases = [b for b in bases if b in names]
+        assert bases
+        return bases[0]
+    pending_map = dict({
+        k: relevant_base(bases) for k, bases in pending
+    })
+    chain = [names[0]]
+    current = pending_map[chain[0]]
+    while True:
+        chain.append(current)
+        current = pending_map[current]
+        if current in chain[0]:
+            return " < ".join(chain + [current])
 
 
 def underscore(name):
@@ -61,4 +81,4 @@ def is_valid_identifier(name):
         return False
     return re.match(
         "[_A-Za-z][_a-zA-Z0-9]*$", name
-    ) and not keyword.iskeyword(name)
+    ) != None and not keyword.iskeyword(name)
---
+++ test_acspec/test_utils.py
@@ -0,0 +1,55 @@
+import pytest
+from collections import OrderedDict
+
+from acspec import utils
+
+
+def test_topological_iteritems():
+    specs = OrderedDict([
+        ("subspec", {":bases": ["spec"]}),
+        ("superspec", {}),
+        ("spec", {":bases": ["superspec"]}),
+    ])
+    assert list(specs.keys()) == ['subspec', 'superspec', 'spec']
+
+    topological_specs = OrderedDict(utils.topological_iteritems(specs))
+    assert list(topological_specs.keys()) == [
+        'superspec', 'spec', 'subspec'
+    ]
+
+
+def test_topological_iteritems_cyclic_inheritance():
+    specs = {
+        "spec": {
+            ":bases": ["superspec"]
+        },
+        "superspec": {
+            ":bases": ["subspec"]
+        },
+        "subspec": {
+            ":bases": ["spec"]
+        }
+    }
+    with pytest.raises(ValueError) as excinfo:
+        list(utils.topological_iteritems(specs))
+
+    assert str(excinfo.value) == \
+        'Cyclic inheritance: subspec < spec < superspec < subspec'
+
+
+def test_underscore():
+    assert utils.underscore("test_model") == "test_model"
+    assert utils.underscore("TestModel") == "test_model"
+
+
+def test_camelize():
+    assert utils.camelize("TestModel") == "TestModel"
+    assert utils.camelize("test_model") == "TestModel"
+
+
+def test_is_valid_identifier():
+    assert utils.is_valid_identifier("") == False
+    assert utils.is_valid_identifier("test model") == False
+    assert utils.is_valid_identifier("test-model") == False
+    assert utils.is_valid_identifier("test_model") == True
+    assert utils.is_valid_identifier("TestModel") == True
